f151264246935e3992e4804a703b40c9ef10f704
VyaxUltima <135572786+VyaxUltima@users.noreply.github.com>
2025-01-11 16:47:53 -0600

    lots of decap

--- src/item_menu.c
+++ src/item_menu.c
@@ -301,12 +301,10 @@ static const u8 sContextMenuItems_BallsPocket[] = {
 };
 
 static const u8 sContextMenuItems_TmHmPocket[] = {
-    ACTION_USE,         ACTION_GIVE,
-    ACTION_DUMMY,       ACTION_CANCEL
+    ACTION_USE,         ACTION_CANCEL
 };
 
 static const u8 sContextMenuItems_BerriesPocket[] = {
-    ACTION_CHECK_TAG,   ACTION_DUMMY,
     ACTION_USE,         ACTION_GIVE,
     ACTION_TOSS,        ACTION_CANCEL
 };
